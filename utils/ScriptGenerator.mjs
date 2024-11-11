ee4fa40104d05526766f4991b7a25f60d354efdf
Glenn Jorde <glenn@tregusti.com>
2024-11-11 10:04:21 +0100

    Try with lftp instead
    But does seem to get the file sizes wrong. Fix or switch to another again?

--- utils/ScriptGenerator.mjs
+++ utils/ScriptGenerator.mjs
@@ -1,13 +1,15 @@
 import { saveFile } from './fileUtils.mjs'
 import { chmod, constants } from 'fs/promises'
 import path from 'path'
+import dedent from 'string-dedent'
 
 import { UpdateType } from './UpdateType.mjs'
+import { unsubscribe } from 'diagnostics_channel'
 
 export class ScriptGenerator {
   #entries = new Array()
 
-  #generateRemoteFilename(type, filename) {
+  static #generateRemoteFilename(type, filename) {
     UpdateType.validate(type)
     if (type === UpdateType.Plugin) {
       return path.join('plugins', filename)
@@ -17,29 +19,43 @@ export class ScriptGenerator {
     }
   }
 
-  #generateEntry([type, filename]) {
+  static #generateEntry([type, filename]) {
     const localFile = path.join(import.meta.dirname, '../downloads', filename)
-    const remoteFile = this.#generateRemoteFilename(type, filename)
-    // console.debug('ScriptGenerator#generateEntry: ', { pathPrefix, localFile, remoteFile })
-    return `ncftpput -f $SECRET_FILE -C "${localFile}" "${remoteFile}"`
+    const remoteFile = ScriptGenerator.#generateRemoteFilename(type, filename)
+    const localBasename = path.basename(localFile)
+    // console.debug('ScriptG${enerator#generateEntry: ', { pathPrefix, localFile, remoteFile })
+
+    return dedent`
+      put -O /default/ '${localFile}' -o '${remoteFile}' &&\\
+      echo 'Uploaded: ${localBasename}' ||\\
+      echo 'Failed upload: ${localBasename}';\\
+    `
+
+    // return `put -O /default/ "${localFile}" -o "${remoteFile}" ; !echo Uploaded: $(basename \\"${localFile}\\")`
+    // return `ncftpput -f $SECRET_FILE -C "${localFile}" "${remoteFile}"`
   }
 
   #generateShellScript() {
-    const commands = []
     if (this.#entries.length > 0) {
-      // See: https://www.ncftp.com/ncftp/doc/ncftpput.html
-      // Example file contents:
-      //   host 5520.node.apexhosting.gdn
-      //   user glenn@tregusti.com.2568190
-      //   pass <PASSWORD HERE>
-      commands.push(
-        '#!/bin/bash',
-        'SECRET_FILE=$(dirname $0)/.apex_secret',
-        '',
-        ...this.#entries.map(this.#generateEntry.bind(this))
-      )
+      const commands = this.#entries.map(ScriptGenerator.#generateEntry).join('\n\\\n')
+      // See: https://lftp.yar.ru/lftp-man.html
+      const username = 'glenn@tregusti.com.2568190'
+      const hostname = '5520.node.apexhosting.gdn'
+      return dedent`
+        #!/bin/bash
+        echo "\\
+        ${commands}
+        " | LFTP_PASSWORD=$(cat $(dirname $0)/.apex_secret) lftp -u "${username}" --env-password ${hostname}
+      `
+
+      // commands.push(
+      //   '#!/bin/bash\n',
+      //   `LFTP_PASSWORD=$(cat $(dirname $0)/.apex_secret) lftp -u "glenn@tregusti.com.2568190" --env-password 5520.node.apexhosting.gdn -c '`,
+      //   ...this.#entries.map(this.#generateEntry.bind(this)).join('\\\n ; '),
+      //   ` ; exit'`
+      // )
     }
-    return commands.join('\n')
+    return null
   }
 
   add(type, filename) {
@@ -48,7 +64,7 @@ export class ScriptGenerator {
   }
   async create() {
     const buffer = this.#generateShellScript()
-    if (buffer.length === 0) {
+    if (!buffer) {
       return { filename: null }
     }
 
