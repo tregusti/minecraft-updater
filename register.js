fe36ea330ea286e6f4f8ee17340bbdf7e8097d45
Glenn Jorde <glenn@tregusti.com>
2024-12-15 00:46:15 +0100

    Use maintained version of ts-node
    To remove some deprecation warnings and simplify importing stuff in node.

--- register.js
+++
@@ -1,6 +0,0 @@
-// https://github.com/nodejs/node/issues/51196#issuecomment-1998216742
-
-import { register } from 'node:module'
-import { pathToFileURL } from 'node:url'
-
-register('ts-node/esm', pathToFileURL('./'))
